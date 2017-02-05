06d9f1e4fa0a28e36bf47760b0258b6cbf51b2b6
Fabio4209 <fabio4209@gmail.com>
2017-02-05 21:03:13 -0200

    adicionado leitura de arquivo

--- src/negocio/fileutil/FileUtil.java
+++ src/negocio/fileutil/FileUtil.java
@@ -1,11 +1,13 @@
 package negocio.fileutil;
 
 import java.io.IOException;
+
 import java.nio.file.StandardOpenOption;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
@@ -16,14 +18,35 @@ public class FileUtil {
 
 	public static void main(String[] args) throws IOException{
 		
-		if (ArquivoNaoExiste(NOME_COMPLETO_ARQUIVO)){
-			criarArquivo(NOME_COMPLETO_ARQUIVO);
-		}
-		else {
-			//escreverNoArquivo(NOME_COMPLETO_ARQUIVO);
-			escreverEmArquivoGrande(NOME_COMPLETO_ARQUIVO);
-		}
 		
+		leArquivo();
+		
+	}
+	
+	public static void leArquivo(){
+		Charset charset = Charset.forName("ISO-8859-1");
+		
+		Path arquivo = Paths.get(".\\246611_unico.txt");
+		
+		try (BufferedReader reader = Files.newBufferedReader(arquivo,charset)) {
+			String line = null;
+			
+			long tempoInicial = System.currentTimeMillis();
+			
+			while ((line = reader.readLine()) != null){
+				System.out.println(line);
+				line = null;
+			}
+			
+			long tempoDecorrido = System.currentTimeMillis() - tempoInicial;
+			
+			System.out.println("Tempo(ms) leitura do arquivo: " + String.valueOf(tempoDecorrido));
+			
+		} catch (IOException ioExcepction){
+			System.err.format("IOException: %s%n", ioExcepction);
+		}
+				
+				
 	}
 	
 	//Path Operations: https://goo.gl/R8IcMu
@@ -51,11 +74,20 @@ public class FileUtil {
 		}
 	}
 	
+	public static void verificaExistenciaArquivo(){
+		if (ArquivoNaoExiste(NOME_COMPLETO_ARQUIVO)){
+			criarArquivo(NOME_COMPLETO_ARQUIVO);
+		}
+		else {
+			escreverEmArquivoTexto(NOME_COMPLETO_ARQUIVO);
+		}
+	}
 	
 	/**
-	 * @descripion Escreve em arquivos maiores, é mais performatico
+	 * Escreve em arquivos de texto.
+	 * Arquivos mais complexos não usam buffer
 	 */	
-	public static void escreverEmArquivoGrande(String nomeArquivo) {
+	public static void escreverEmArquivoTexto(String nomeArquivo) {
 		Path arquivo = Paths.get(nomeArquivo);
 
 		Charset charset = Charset.forName("US-ASCII");
